a9b37476aebf7eb2c2df2717048d71e6b16cbe12
double2kill <379563000@qq.com>
2020-09-25 17:30:53 +0800

    fix button style in audio preview page

--- jira.js
+++ jira.js
@@ -12,7 +12,7 @@ var copyCommitInfo = () => {
 var copyButton = document.createElement("div")
 
 var svg = '<svg t="1600058189079" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="1854" xmlns:xlink="http://www.w3.org/1999/xlink" width="200" height="200"><defs><style type="text/css"></style></defs><path d="M969.152 219.424q22.848 0 38.848 16t16 38.848l0 694.848q0 22.848-16 38.848t-38.848 16l-548.576 0q-22.848 0-38.848-16t-16-38.848l0-164.576-310.848 0q-22.848 0-38.848-16t-16-38.848l0-384q0-22.848 11.424-50.272t27.424-43.424l233.152-233.152q16-16 43.424-27.424t50.272-11.424l237.728 0q22.848 0 38.848 16t16 38.848l0 187.424q38.848-22.848 73.152-22.848l237.728 0zM658.272 341.152l-170.848 170.848 170.848 0 0-170.848zM292.576 121.728l-170.848 170.848 170.848 0 0-170.848zM404.576 491.424l180.576-180.576 0-237.728-219.424 0 0 237.728q0 22.848-16 38.848t-38.848 16l-237.728 0 0 365.728 292.576 0 0-146.272q0-22.848 11.424-50.272t27.424-43.424zM950.848 950.848l0-658.272-219.424 0 0 237.728q0 22.848-16 38.848t-38.848 16l-237.728 0 0 365.728 512 0z" p-id="1855" fill="#ffffff"></path></svg>'
-var buttonText = svg + ' ' + '复制 commit 信息'
+var buttonText = svg + '<span class="space"> </span>' + '复制 commit 信息'
 copyButton.id = 'jira-extension-copy'
 copyButton.className = 'ant-btn ant-btn-primary'
 copyButton.innerHTML = buttonText
