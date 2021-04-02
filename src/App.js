e1e2883f71edfd43ff97a256af899de87dd53f39
double2kill <379563000@qq.com>
2021-04-02 16:19:18 +0800

    feat: 🎸 当主面板拖到屏幕外面时，可以缩小

--- src/App.js
+++ src/App.js
@@ -2,15 +2,46 @@ import {reactive, ref} from 'vue'
 import Localstorage from 'localstorage'
 
 export const jiraPositionLocalStorage = new Localstorage('JIRA_POSITION')
-export const position = reactive({ x: 0, y: 0})
+export const position = reactive({ x: 0, y: 0, touchBorder: 'none', dragging: false})
 export const mainButtonRef = ref(null)
+export const isHoverAtContainerWhenLeave = ref(false)
 
+const DETECT_TOLERANCE = 10
 let diffY, diffX
-const adjustPositionInWindow = () => {
+
+const getMaxInfo = () => {
   const {width, height} = mainButtonRef.value.getBoundingClientRect()
   const {offsetWidth, offsetHeight} = document.body
   const maxY = offsetHeight - height
   const maxX = offsetWidth - width
+  return {
+    maxX, maxY
+  }
+}
+
+const checkTouchWindowBorder = () => {
+  const {maxX, maxY} = getMaxInfo()
+  if(position.y === 0) {
+    position.touchBorder = 'top'
+    return
+  }
+  if(position.x === maxX) {
+    position.touchBorder = 'right'
+    return
+  }
+  if(position.y === maxY) {
+    position.touchBorder = 'bottom'
+    return
+  }
+  if(position.x === 0) {
+    position.touchBorder = 'left'
+    return
+  }
+  position.touchBorder = 'none'
+}
+
+const adjustPositionInWindow = () => {
+  const {maxX, maxY} = getMaxInfo()
   // move to top of window
   position.y = Math.max(position.y, 0)
   // move to right of window
@@ -22,14 +53,17 @@ const adjustPositionInWindow = () => {
 }
 
 export const onMove = (event) => {
+  position.dragging = true
   position.y = event.clientY - diffY
   position.x = event.clientX - diffX
   adjustPositionInWindow()
 }
 
 export const onEnd = (event) => {
+  position.dragging = false
   document.removeEventListener('pointermove', onMove)
   document.removeEventListener('mouseup', onEnd)
+  checkTouchWindowBorder()
   jiraPositionLocalStorage.put(undefined, position)
   event.stopPropagation()
 }
@@ -41,3 +75,26 @@ export const onStart = (event) => {
   diffX = event.clientX - position.x
   document.body.classList.add('no-select')
 }
+
+document.addEventListener('mouseleave', (event) => {
+  // 当鼠标移出窗口时，hover不会触发，所以需要多一个状态来标记 Hover .
+  const {clientX, clientY} = event
+  const {width, height} = mainButtonRef.value.getBoundingClientRect()
+
+  const insideAtX = clientX > position.x && clientX < position.x + width
+  const insideAtY = clientY > position.y && clientY < position.y + height
+
+  const isLeaveFromTop = insideAtX && clientY < DETECT_TOLERANCE && position.touchBorder === 'top'
+  const isLeaveFromRight = insideAtY && clientX > width - DETECT_TOLERANCE && position.touchBorder === 'right'
+  const isLeaveFromBottom = insideAtX && clientY > height - DETECT_TOLERANCE && position.touchBorder === 'bottom'
+  const isLeaveFromLeft = insideAtY && clientX < DETECT_TOLERANCE && position.touchBorder === 'left'
+
+  if(isLeaveFromTop || isLeaveFromRight || isLeaveFromBottom || isLeaveFromLeft) {
+    isHoverAtContainerWhenLeave.value = true
+    return 
+  }
+})
+
+document.addEventListener('mouseenter', () => {
+  isHoverAtContainerWhenLeave.value = false
+})
--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,16 @@
   <div
     ref="mainButtonRef"
     class="draggble"
+    :class="{
+      'touch-top': position.touchBorder === 'top' && position.dragging === false,
+      'touch-right': position.touchBorder === 'right' && position.dragging === false,
+      'touch-bottom': position.touchBorder === 'bottom' && position.dragging === false,
+      'touch-left': position.touchBorder === 'left' && position.dragging === false,
+      'hover-when-leave': isHoverAtContainerWhenLeave,
+    }"
     :style="{
       left: `${position.x}px`,
-      top: `${position.y}px`
+      top: `${position.y}px`,
     }"
   >
     <a-card
@@ -14,13 +21,13 @@
       head-style="text-align: center;"
     >
       <template #title>
-        <span
+        <div
           class="jira-extension-card__title"
           @mousedown="onStart"
         >
           <DragOutlined />
           jira 插件
-        </span>
+        </div>
       </template>
       <CopyJiraButton />
       <DrawerButton />
@@ -29,27 +36,35 @@
 </template>
 
 <script setup>
-import {onMounted} from 'vue'
-import {DragOutlined} from '@ant-design/icons-vue'
-import CopyJiraButton from './components/CopyJiraButton.vue'
-import DrawerButton from './components/Drawer/DrawerButton.vue'
-import {position, mainButtonRef, onMove, onEnd, onStart, jiraPositionLocalStorage} from './App'
+import { onMounted } from "vue"
+import { DragOutlined } from "@ant-design/icons-vue"
+import CopyJiraButton from "./components/CopyJiraButton.vue"
+import DrawerButton from "./components/Drawer/DrawerButton.vue"
+import {
+  position,
+  mainButtonRef,
+  onMove,
+  onEnd,
+  onStart,
+  jiraPositionLocalStorage,
+  isHoverAtContainerWhenLeave,
+} from "./App"
 
 onMounted(() => {
   const [error, positionValue] = jiraPositionLocalStorage.get()
   if (positionValue) {
     position.x = positionValue.x
     position.y = positionValue.y
+    position.touchBorder = positionValue.touchBorder
     return
   }
   const INITIAL_RIGHT = 50
   const INITIAL_BOTTOM = 50
-  const {offsetWidth, offsetHeight} = document.body
-  const {width, height} = mainButtonRef.value.getBoundingClientRect()
+  const { offsetWidth, offsetHeight } = document.body
+  const { width, height } = mainButtonRef.value.getBoundingClientRect()
   position.x = offsetWidth - width - INITIAL_RIGHT
   position.y = offsetHeight - height - INITIAL_BOTTOM
 })
-
 </script>
 
 <style>
@@ -59,9 +74,16 @@ onMounted(() => {
 .draggble {
   position: fixed;
   z-index: 1000;
+  transition: transform 0.5s;
 }
-.jira-extension-card{
+.jira-extension-card {
   box-shadow: 0 2px 8px rgb(0 0 0 / 18%);
+}
+.jira-extension-card.ant-card-small > .ant-card-head {
+  padding: 0;
+}
+.jira-extension-card.ant-card-small > .ant-card-head > .ant-card-head-wrapper > .ant-card-head-title {
+  padding: 0;
 }
 .jira-extension-card .ant-card-body {
   display: flex;
@@ -72,5 +94,29 @@ onMounted(() => {
 }
 .jira-extension-card__title {
   cursor: pointer;
+  padding: 8px 0;
+}
+
+.touch-top {
+  transform: translateY(-95%);
+}
+.touch-right {
+  transform: translateX(95%);
+}
+.touch-bottom {
+  transform: translateY(95%);
+}
+.touch-left {
+  transform: translateX(-95%);
+}
+
+.draggble:hover {
+  transform: translateX(0%);
+  transform: translateY(0%);
+}
+
+.hover-when-leave {
+  transform: translateX(0%);
+  transform: translateY(0%);
 }
 </style>
